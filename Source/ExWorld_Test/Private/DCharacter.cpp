906e77998683819dfd3baed98e21ea156fb13e45
DylanJamesRamsden <dylan.james.ramsden@gmail.com>
2022-01-21 06:28:31 +0200

    DCharacter cleaned up.

--- Source/ExWorld_Test/Private/DCharacter.cpp
+++ Source/ExWorld_Test/Private/DCharacter.cpp
@@ -12,19 +12,22 @@ ADCharacter::ADCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	//The spring arm component is created and attached to the root component of the character.
 	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
 	SpringArmComp->bUsePawnControlRotation = true;
 	SpringArmComp->SetupAttachment(RootComponent);
-	
+
+	//The camera component is created and attached to the spring arm component.
 	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
 	CameraComp->SetupAttachment(SpringArmComp);
 
+	//A spell component is created. This component will handle the spawning of a projectile and it's cooldown.
 	SpellComponent = CreateDefaultSubobject<UDSpellComponent>("SpellComp");
 	
-	//Rotates the character to face the direction in which they are moving
+	//Rotates the character to face the direction in which they are moving.
 	GetCharacterMovement()->bOrientRotationToMovement = true;
 
-	//Prevents the character from inheriting its controller's Yaw rotation
+	//Prevents the character from inheriting its controller's Yaw rotation.
 	bUseControllerRotationYaw = false;
 }
 
@@ -33,6 +36,7 @@ void ADCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 
+	//Binds the function that applies the damage to our character to the OnTakeAnyDamage signature.
 	OnTakeAnyDamage.AddDynamic(this, &ADCharacter::TakeDamage);
 }
 
@@ -51,7 +55,7 @@ void ADCharacter::MoveRight(float Value)
 	ControllerRot.Pitch = 0.0f;
 	ControllerRot.Roll = 0.0f;
 
-	//Gets the right vector of the pawn's controller (using the Y axis)
+	//Gets the right vector of the pawn's controller (using the Y axis).
 	FVector RightVector = FRotationMatrix(ControllerRot).GetScaledAxis(EAxis::Y);
 	
 	AddMovementInput(RightVector, Value);
@@ -59,11 +63,15 @@ void ADCharacter::MoveRight(float Value)
 
 void ADCharacter::PrimaryAttack()
 {
+	//A primary attack must be called on the server, so if the server has not called it then a server RPC of the primary attack is forced.
 	if (HasAuthority())
 	{
-		if (SpellComponent->IsSpellAvailable())
+		if (SpellComponent)
 		{
-			SpellComponent->CastSpell();
+			if (SpellComponent->IsSpellAvailable())
+			{
+				SpellComponent->CastSpell();
+			}	
 		}
 	}
 	else
@@ -77,51 +85,25 @@ void ADCharacter::ServerPrimaryAttack_Implementation()
 	PrimaryAttack();
 }
 
-/*void ADCharacter::SpawnProjectile()
-{	
-	FVector HandLocation = GetMesh()->GetSocketLocation(PrimaryAttackSocket);
-
-	//Spawn Transform Matrix
-	FTransform SpawnTM = FTransform(GetControlRotation() ,HandLocation);
-
-	FActorSpawnParameters SpawnParams;
-	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
-	SpawnParams.Instigator = this;
-	
-	GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
-}*/
-
-/*void ADCharacter::ClientPlayAttackAnimation_Implementation()
-{
-	PlayAnimMontage(AttackAnim);
-}*/
-
 // Called every frame
 void ADCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
 
-	/*if (!bCanAttack)
+void ADCharacter::TakeDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser)
+{
+	ADPlayerState* OwningPlayerState = Cast<ADPlayerState>(GetPlayerState());
+
+	if (OwningPlayerState)
 	{
-		if (HasAuthority())
-		{
-			CurrentCooldown++;
-			if (CurrentCooldown >= AttackCooldown)
-			{
-				bCanAttack = true;
-			}
-		}
+		OwningPlayerState->RemoveHealth(Damage);
+	}
+}
 
-		if (!IsRunningDedicatedServer() && CurrentCooldown < AttackCooldown)
-		{
-			//The cooldown is predicted on the client
-			if (!HasAuthority())
-			{
-				CurrentCooldown++;
-			}
-		}
-		
-	}*/
+UDSpellComponent* ADCharacter::GetSpellComponent() const
+{
+	return SpellComponent;
 }
 
 // Called to bind functionality to input
@@ -138,20 +120,5 @@ void ADCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ADCharacter::Jump);
 
 	PlayerInputComponent->BindAction("PrimaryAttack", IE_Pressed, this, &ADCharacter::PrimaryAttack);
-}
-
-void ADCharacter::TakeDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser)
-{
-	ADPlayerState* OwningPlayerState = Cast<ADPlayerState>(GetPlayerState());
-
-	if (OwningPlayerState)
-	{
-		OwningPlayerState->RemoveHealth(Damage);
-	}
-}
-
-UDSpellComponent* ADCharacter::GetSpellComponent() const
-{
-	return SpellComponent;
 }
 
--- Source/ExWorld_Test/Public/DCharacter.h
+++ Source/ExWorld_Test/Public/DCharacter.h
@@ -26,22 +26,24 @@ protected:
     
     UPROPERTY(VisibleAnywhere)
     USpringArmComponent* SpringArmComp;
-    
+
+	/*The spell component is responsible for spawning a projectile when the character attacks as well as handling the cooldown timer.*/
     UPROPERTY(VisibleAnywhere)
 	UDSpellComponent* SpellComponent;
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	//Moves the character forward and backwards
+	/*Move the character forward and backwards based on the provided axis.*/
 	void MoveForward(float Value);
 
-	//Moves the character left and right
+	/*Move the character left and right based on the provided axis.*/
 	void MoveRight(float Value);
 
-	//Initiates a primary attack timer and animation
+	/*Initiates a primary attack.*/
 	void PrimaryAttack();
 
+	/*Initiate a primary attack on the server.*/
 	UFUNCTION(Server, Reliable)
 	void ServerPrimaryAttack();
 
@@ -51,10 +53,12 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	
+
+	/*Apply damage to the character. This function is bound to OnTakeAnyDamage.*/
 	UFUNCTION(BlueprintCallable)
 	void TakeDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
-
+	
+	/*Get the spell component of the character.*/
 	UFUNCTION(BlueprintPure)
 	UDSpellComponent* GetSpellComponent() const;
 };
